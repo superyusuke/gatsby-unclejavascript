0626a5e6be06aadb303bdd4087ffef0096ecd452
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2018-10-14 15:10:55 +0900

    CSS Module の導入

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 
 import Layout from '../components/layout'
 
+import testStyles from '../components/test.module.css'
+
 const Header = styled.h1`
   color: red;
 `
@@ -11,7 +13,7 @@ const Header = styled.h1`
 const IndexPage = () => (
   <Layout>
     <Header>Atwood の法則</Header>
-    <p>
+    <p className={testStyles.test}>
       JavaScript で書けるアプリケーションは全て、最終的に JavaScript
       で書かれることになっている。
     </p>
