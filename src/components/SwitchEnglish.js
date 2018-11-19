0e433a920c680a1a9cbc8f0a461e3be58661608b
Nakanishi, Yusuke <super.yusuke0000@gmail.com>
2018-11-19 21:24:03 +0900

    英語表示切り替えコンポーネント

---
+++ src/components/SwitchEnglish.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import classNames from 'classnames'
+import 'src/components/switch-english.scss'
+
+class SwitchEnglish extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { english: true }
+  }
+
+  none = () => {
+    const quote = document.querySelectorAll('blockquote')
+    quote.forEach(o => (o.style.display = 'none'))
+    this.setState({ english: true })
+    console.log(this.state.english)
+  }
+
+  block = () => {
+    const quote = document.querySelectorAll('blockquote')
+    quote.forEach(o => (o.style.display = 'block'))
+    this.setState({ english: false })
+    console.log(this.state.english)
+  }
+
+  buttonStyle(bool) {
+    return classNames('switch-english__button', {
+      'switch-english__button--selected': bool,
+    })
+  }
+
+  render() {
+    const { translatedMode } = this.props
+    if (!translatedMode) {
+      return null
+    }
+    return (
+      <div className="switch-english">
+        <button
+          className={this.buttonStyle(this.state.english)}
+          onClick={this.none}
+        >
+          英語非表示
+        </button>
+        <button
+          className={this.buttonStyle(!this.state.english)}
+          onClick={this.block}
+        >
+          英語表示
+        </button>
+      </div>
+    )
+  }
+}
+
+export default SwitchEnglish
