e5a202f985d7799c47a03a40452ce03caa8c8612
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-03-06 16:15:19 +0900

    平和がランダムで発動

--- src/components/Peace.js
+++ src/components/Peace.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Random } from 'random-js'
+
+const random = new Random() // uses the nativeMath engine
 
 class Peace extends React.Component {
   componentDidMount() {
-    while (true) {
-      alert(`
+    const randomValue = random.integer(1, 100)
+
+    if (randomValue < 10) {
+      while (true) {
+        alert(`
     “War is peace.
     Freedom is slavery.
     Ignorance is strength.” `)
+      }
     }
   }
   render() {
